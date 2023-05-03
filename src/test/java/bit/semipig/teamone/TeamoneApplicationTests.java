090cfcf0359a5c05f0fcdde60726f455d75ebeb2
K1mSH <124352219+K1mSH@users.noreply.github.com>
2023-05-03 16:00:18 +0900

    1600

---
+++ src/test/java/bit/semipig/teamone/TeamoneApplicationTests.java
@@ -0,0 +1,13 @@
+package bit.semipig.teamone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
