0e002fdfeb74c0d7fd9d4132f9760ffa6f8d0d85
K1mSH <124352219+K1mSH@users.noreply.github.com>
2023-05-16 16:30:31 +0900

    0430 commit
    small review 지우기

---
+++ src/main/java/boot/semipig/mapper/ServiceMapper.java
@@ -0,0 +1,28 @@
+package boot.semipig.Mapper;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import boot.semipig.dto.ServiceDto;
+import boot.semipig.dto.couponDto;
+import boot.semipig.dto.qnaDto;
+import boot.semipig.dto.ReviewDto;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ServiceMapper {
+
+    public void insertt(ServiceDto dto);
+    public List<ServiceDto> selectt();
+    public void deletee(int num);
+    public List<ServiceDto> getAll(Map<String, Object> map);
+    public void updatee(ServiceDto dto);
+    public List<couponDto> couponall();
+    public void couponinsert(couponDto dto);
+    public void deletecoupon(int num);
+    public int getTotalCount();
+    public List<qnaDto> qnalist(Map<String, Object> map);
+    public List<ReviewDto> reviewlist(Map<String, Object> map);
+
+    int ReservationCount(String start);
+}
